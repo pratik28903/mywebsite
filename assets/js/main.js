b083364bfd3a45e534443687e8be88c0978008cb
Pratik Ostwal <75239298+pratik28903@users.noreply.github.com>
2021-04-14 15:49:45 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -123,3 +123,12 @@ sr.reveal('.work__img',{interval: 200});
 
 /*SCROLL CONTACT*/
 sr.reveal('.contact__input',{interval: 200});
+
+/*==================== SHOW SCROLL TOP ====================*/ 
+function scrollTop(){
+  const scrollTop = document.getElementById('scroll-top');
+  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if(this.scrollY >= 200) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
+
